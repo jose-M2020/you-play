f4909d87113787b2e0f36b9eb43bd01f63b7b662
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2022-10-13 14:27:27 -0500

    [add] Search results section

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
+        <Route path="/" element={<Feed />} exact />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" eLement={<SearchFeed />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
 );
- export default App;
+
+export default App;
